30e9ace56a8ad63db323e0ca71f90417d9f1af39
MaximKiselyow <m.kiselyow@gmail.com>
2018-03-12 05:24:45 +0200

    done

--- lib/file_transformer.rb
+++ lib/file_transformer.rb
@@ -1,5 +1,32 @@
+require 'yaml/store'
+
 class FileTransformer
   def self.transform(file_path)
-    File.open(file_path, 'r').read
+    file = File.open(file_path, 'r')
+    lines = file.read.to_s.split("\n")
+    answer = []
+    lines.map do |line|
+      value = line.split(': ')[1]
+      key = line.split(': ')[0].gsub(/'/, '')
+      keys_array = key.split('.')
+      result = {}
+      answer << get_key_and_value_from_keys_array(keys_array, value, result)
+    end
+    File.open('translations.yml', 'w+').write(answer.to_yaml)
+    file.close
+    answer.to_yaml
   end
+
+  def self.get_key_and_value_from_keys_array(keys_array, value, result)
+    keys_array.map do |key|
+      if keys_array.length > 1
+        keys_array.shift
+        result[key] = {}
+        { key => get_key_and_value_from_keys_array(keys_array, value, result[key])}
+      else
+        { key => value}
+      end
+    end
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,24 @@ require './lib/file_transformer'
 RSpec.describe FileTransformer do
   describe "transform file method" do
     it "returns transformed file" do
-      expect(described_class.transform("./lib/translations_simple.yml")).to eq('')
+      expect(described_class.transform("./lib/translations_simple.yml")).to eq(
+        {
+          'en' => {
+            'pets' => {
+              'types' => {
+                'cat' => 'Cat',
+                'dog' => 'Dog'
+              }
+            },
+            'title' => 'My lovely pets',
+            'actions' => {
+              'add' => 'Add',
+              'remove' => 'Remove'
+            },
+            'language' => '<strong>Language</strong>'
+          }
+        }.to_yaml
+      )
     end
   end
 end
